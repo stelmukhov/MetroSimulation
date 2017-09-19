cac1fae445a24e6b1a66a0fd5a909bb380707be9
SpinStrike <antonbezverkhy@mail.com>
2017-09-19 20:28:35 +0300

    Train class v1.0.0.0

---
+++ MetroSimulation.Train/Train.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MetroSimulation.Train
+{
+    public class Train
+    {
+        public int MaxPassengers { get; private set; }
+
+        public Train(string trainNumber, float speed, int maxPassengers)
+        {
+            _trainNumber = trainNumber;
+            _speed = speed;
+            MaxPassengers = maxPassengers;
+            _currentPassengers = 0;
+        }
+
+        public Train(int trainNumber, float speed, int maxPassengers)
+        {
+            _trainNumber = trainNumber.ToString();
+            _speed = speed;
+            MaxPassengers = maxPassengers;
+            _currentPassengers = 0;
+        }
+
+        public string Number
+        {
+            get { return _trainNumber; }
+        }
+
+        public float Speed
+        {
+            get { return _speed; }
+        }
+
+        public int CurrentPassengers
+        {
+            get { return _currentPassengers; }
+        }
+
+        private string _trainNumber;
+        private float  _speed;
+        private int    _currentPassengers;
+    }
+}
--- Train/Class1.cs
+++ Train/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MetroSimulation
+namespace Train
 {
     public class Class1
     {
