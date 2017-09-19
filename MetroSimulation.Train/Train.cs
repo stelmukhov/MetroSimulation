f7f462c1b3596cdcafd50eb0d0c348724eed5e75
SpinStrike <antonbezverkhy@mail.com>
2017-09-19 21:45:55 +0300

    Changes: in class Train deleted 1 ctor, added class TrainInfo, added wcf TrainService.

--- MetroSimulation.Train/Train.cs
+++ MetroSimulation.Train/Train.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using System.Runtime.Serialization;
 
 namespace MetroSimulation.Train
 {
+    [DataContract]
     public class Train
     {
+        [DataMember]
         public int MaxPassengers { get; private set; }
 
         public Train(string trainNumber, float speed, int maxPassengers)
@@ -14,28 +16,29 @@ namespace MetroSimulation.Train
             _currentPassengers = 0;
         }
 
-        public Train(int trainNumber, float speed, int maxPassengers)
-        {
-            _trainNumber = trainNumber.ToString();
-            _speed = speed;
-            MaxPassengers = maxPassengers;
-            _currentPassengers = 0;
-        }
-
+        [DataMember]
         public string Number
         {
             get { return _trainNumber; }
         }
 
+        [DataMember]
         public float Speed
         {
             get { return _speed; }
         }
 
+        [DataMember]
         public int CurrentPassengers
         {
             get { return _currentPassengers; }
         }
+
+        public void SetPassengers(int passengers)
+        {
+            _currentPassengers = passengers;
+        }
+
 
         private string _trainNumber;
         private float  _speed;
---
+++ MetroSimulation.Train/TrainInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace MetroSimulation.Train
+{
+    public class TrainInfo
+    {
+        public Train  BaseTrain { get; private set; }
+        public string TrainPosition { get; private set;}
+
+        public TrainInfo(string trainNumber, float speed, int maxPassengers)
+        {
+            BaseTrain = new Train(trainNumber, speed, maxPassengers);
+        }
+
+
+        public void SetNewPosition(string newPosition)
+        {
+            TrainPosition = newPosition;
+        }
+    }
+}
