c211b32957c7f00d54904dd9e1aeadd31ed08ca5
SpinStrike <antonbezverkhy@mail.com>
2017-10-21 18:52:34 +0300

    Added in  Data layer a Station hierarchy.

---
+++ MetroSimulation.TrainStation/MetroDepot.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroSimulation.TrainStation
+{
+    public class MetroDepot : Station
+    {
+        public MetroDepot(string name, double depotDelay)
+            :base (10, name, depotDelay)
+        {
+            trains = new List<string>(10);
+            topLine = new List<string>(1);
+            bottomLine = new List<string>(1);
+        }
+
+
+        public void AddTrain(string train)
+        {
+            trains.Add(train);
+        }
+
+        public IEnumerable<string> TrainsInDepot
+        {
+            get { return trains; }
+        }
+
+        public string OutTunnel
+        {
+            get { return bottomLine.ElementAt(0); }
+            set { bottomLine[0] = value; }
+        }
+
+        public string InTunnel
+        {
+            get { return topLine.ElementAt(0); }
+            set { topLine[0] = value; }
+        }
+    }
+}
---
+++ MetroSimulation.TrainStation/MetroTunnel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroSimulation.TrainStation
+{
+    public class MetroTunnel : Station
+    {
+        public MetroTunnel(string name, double tunnelDelay)
+            :base(1, name, tunnelDelay)
+        {
+            trains = new List<string>(1);
+            topLine = new List<string>(2);
+            bottomLine = null;
+        }
+
+        public string InStation
+        {
+            get { return topLine.ElementAt(0); }
+            set { topLine[0] = value; }
+        }
+
+        public string OutStation
+        {
+            get { return topLine.ElementAt(1); }
+            set { topLine[1] = value; }
+        }
+
+        public string TrainInTunnel
+        {
+            get { return trains.ElementAt(0); }
+            set { trains[0] = value; }
+        }
+       
+
+    }
+}
