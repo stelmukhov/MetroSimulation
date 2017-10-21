0fb8625d1455c53454a4f00b6cd257f120efe854
SpinStrike <antonbezverkhy@mail.com>
2017-10-21 19:51:46 +0300

    Basic functional of Depot Service.

---
+++ MetroSimulation.DepotService/DepotService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using MetroSimulation.TrainStation;
+
+namespace MetroSimulation.DepotService
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, InstanceContextMode = InstanceContextMode.Single)]
+    public class DepotService : IDepotService
+    {
+        public DepotService(string name, double delay)
+        {
+            _depot = new MetroDepot(name, delay);
+        }
+
+    
+        public double GetDepotDelay()
+        {
+            return _depot.DelayStation;
+        }
+
+        public string GetInTunnel()
+        {
+            return _depot.InTunnel;
+        }
+
+        public string GetOutTunnel()
+        {
+            return _depot.OutTunnel;
+        }
+
+        public void SetInTunnel(string inTunnel)
+        {
+            _depot.InTunnel = inTunnel;
+        }
+
+        public void SetOutTunnel(string outTunnel)
+        {
+            _depot.OutTunnel = outTunnel;
+        }
+
+        public void SetTrain(string train)
+        {
+            ((List<string>)_depot.TrainsInDepot).Add(train);
+        }
+
+        public IEnumerable<string> TrainsInDepot()
+        {
+            return _depot.TrainsInDepot;
+        }
+
+        public void SetDepotName(string name)
+        {
+            _depot.Name = name;
+        }
+
+        public string GetDepotName()
+        {
+            return _depot.Name;
+        }
+
+
+        private MetroDepot _depot;
+    }
+}
---
+++ MetroSimulation.DepotService/IDepotService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MetroSimulation.DepotService
+{
+    
+    [ServiceContract]
+    public interface IDepotService
+    {
+        [OperationContract]
+        string GetDepotName();
+
+        [OperationContract]
+        void SetDepotName(string name);
+
+        [OperationContract]
+        void SetTrain(string train);
+
+        [OperationContract]
+        IEnumerable<string> TrainsInDepot();
+
+        [OperationContract]
+        void SetInTunnel(string inTunnel);
+
+        [OperationContract]
+        string GetInTunnel();
+
+        [OperationContract]
+        void SetOutTunnel(string outTunnel);
+
+        [OperationContract]
+        string GetOutTunnel();
+
+        [OperationContract]
+        double GetDepotDelay();
+        
+    }
+}
